14db1ce5119488345dde9e590c656122ad74060f
PopovKonstantin <zamposad@gmail.com>
2024-12-01 14:22:37 +0300

    add creating and showing contacts

--- contacts.py
+++ contacts.py
@@ -14,18 +14,37 @@ class ContactManager:
         self.contacts = []
 
     def save_contacts(self):
-        pass
-    
+        data = [c.__dict__ for c in self.contacts]
+        save_data('contacts.json', data)
+            
+    def load_contacts(self):
+        data = load_data('contacts.json', [])
+        self.contacts = [Contact(**c) for c in data]
+        
+    def create_contact(self, name, phone, email):
+        contact_id = max([contact.contact_id for contact in self.contacts], default=0) + 1
+        contact = Contact(contact_id, name, phone, email)
+        self.contacts.append(contact)
+        self.save_contacts()
+        
+    def show_contacts(self):
+        print('Список контактов:')
+        for contact in self.contacts:
+            print(f'ID: {contact.contact_id}, Имя: {contact.name}, Телефон: {contact.phone}, Email: {contact.email}')
 
+contacts_manager = ContactManager()
 
 def menu_contacts():
     while True:
         print('Управление контактами: \n 1. Добавить контакт \n 2. Посмотреть контакты \n 3. Редактировать контакт \n 4. Удалить контакт \n 5. Найти контакт по имени или номеру телефона \n 6. Экспортировать в CSV \n 7. Импортировать задачи из CSV \n 8. Вернуться в главное меню')
         choice = input('Выберите действие:')
         if choice == '1':
-            pass
+            name = input('Введите имя контакта: ')
+            phone = input('Введите номер телефона: ')
+            email = input('Введите email: ')
+            contacts_manager.create_contact(name, phone, email)
         elif choice == '2':
-            pass
+            contacts_manager.show_contacts()
         elif choice == '3':
             pass
         elif choice == '4':
